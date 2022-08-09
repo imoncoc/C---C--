cdaa5ee23b27d6af98d83b2b79bc97c21e2f2e62
Md Imon Hossain <imoncoc0@gmail.com>
2022-08-09 23:07:12 +0600

    Codeforces solved.

---
+++ Codeforces/82A - Double Cola.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int Sheldon = 0, Leonard = 0, Penny = 0, Rajesh = 0, Howard = 0;
+
+    for(int i = 0; i < n; i++){
+
+    }
+
+
+
+
+
+    return 0;
+}
