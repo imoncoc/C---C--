2e8a029d48b3342f46ea236fb46ee54d9a1d6986
Md Imon Hossain <imoncoc0@gmail.com>
2022-08-20 23:48:29 +0600

    Codeforces Education

---
+++ Codeforces Education/C. Closest to the Right.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, k;
+    cin>>n>>k;
+    vector<int> arr(n);
+
+
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    for(int i = 0; i < k; i++){
+            int target;
+    cin>>target;
+        int left = -1, right = n;
+        while(right > left+1){
+            int mid = (left + right)/2;
+
+            if(arr[mid] < target){
+                left = mid;
+            }else{
+                right = mid;
+            }
+        }
+
+        cout<<right+1<<endl;
+    }
+
+
+    return 0;
+}
+
