e1ad1fa0407f25a6e1fb53a3faa25c67de8c0a89
Md Imon Hossain <imoncoc0@gmail.com>
2022-10-05 10:37:22 +0600

    Codeforces 1739A

---
+++ Codeforces/1739A. Immobile Knight.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n, m;
+    cin>>n>>m;
+    n++, m++;
+
+    cout<<n/2<<" "<<m/2<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
