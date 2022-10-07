5b750ef8666b9967db59871f758e78ded9ffe797
Md Imon Hossain <imoncoc0@gmail.com>
2022-10-07 11:35:20 +0600

    1739B

--- Codeforces/1739B - Array Recovery.cpp
+++ Codeforces/1739B - Array Recovery.cpp
@@ -2,38 +2,45 @@
 using namespace std;
 void solve(){
     int n;
-    cin>>n;
-    vector<int> arr(n);
-    bool flag = false;
+	    cin>>n;
+	    int d[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>d[i];
+	    }
 
-    for(int i = 0; i < n; i++ ){
-        cin>>arr[i];
-    }
+	    int a[n];
+	    a[0]=d[0];
+	    bool ans=false;
+	    for(int i=1;i<n;i++)
+	    {
+	        int temp1 = a[i-1] - d[i];
+	        int temp2 = a[i-1] + d[i];
+	        if(temp1 == temp2 || temp1<0)
+	        {
+	            ans = true;
+	            a[i] = a[i-1] + d[i];
 
-    for(int i = 1; i < n-1; i++){
-            if(arr[i] > arr[i+1] && arr[i+1] != 0){
-                flag = true;
-            }
-            cout<<"arr["<<i<<"]: "<<arr[i]<<"   arr["<<i+1<<"]: "<<arr[i+1]<<endl;
-    }
+	        }
+	        else
+	        {
+	            ans = false;
+	            break;
+	        }
+	    }
 
-
-
-
-
-    if(flag){
-        cout<<-1<<endl;
-    }
-    else{
-            for(int i = 1; i < n; i++){
-        arr[i] = arr[i-1] + arr[i];
-    }
-
-        for(int i = 0; i < n; i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    }
+	    if(ans == false && n!=1)
+	    {
+	        cout<<-1<<endl;
+	    }
+	    else
+	    {
+	        for(int i = 0; i < n; i++)
+	        {
+	            cout<<a[i]<<" ";
+	        }
+	        cout<<endl;
+	    }
 }
 int main(){
     int t;
