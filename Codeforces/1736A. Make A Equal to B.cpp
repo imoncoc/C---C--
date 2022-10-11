6478eeabbc2f38b8bf6dec4986de3c1d22cfe7dc
Md Imon Hossain <imoncoc0@gmail.com>
2022-10-11 21:49:05 +0600

    1736A

--- Codeforces/1736A. Make A Equal to B.cpp
+++ Codeforces/1736A. Make A Equal to B.cpp
@@ -1,34 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 void solve(){
-    int n;
-    cin>>n;
-    vector<int> arr1(n), arr2(n);
-    int count = 0;
+    ll n; cin>>n;
+    ll sum = 0, ans = 0;
+    vector<ll> a(n), b(n);
 
-    for(int i = 0; i < n; i++) cin>>arr1[i];
-    for(int i = 0; i < n; i++) cin>>arr2[i];
-
-    for(int i = 0; i < n; i++){
-        if(arr1[i] == arr2[i]) count++;
+    for(auto &it: a){
+        cin>>it;
+        sum+=it;
+    }
+    for(auto &it: b){
+        cin>>it;
+        sum-=it;
     }
 
-    if(count == n){
-        cout<<0<<endl;
+    for(ll i = 0; i < n; i++){
+        ans += (a[i] ^ b[i]);
     }
-    else{
-        sort(arr1.begin(), arr1.end());
-        sort(arr2.begin(), arr2.end());
-        count = 1;
-        for(int i = 0; i < n; i++){
-            if(arr1[i] != arr2[i]){
-                count++;
-            }
-        }
 
-        //cout<<"ans: "<<count<<endl;
-        cout<<count<<endl;
-    }
+    ans = min(ans, 1+abs(sum));
+    cout<<ans<<"\n";
 }
 int main(){
     int t;
