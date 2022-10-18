cfb2f4202223abafda220aa5f33c2cc0b60a8ec8
Md Imon Hossain <imoncoc0@gmail.com>
2022-10-18 09:40:43 +0600

    Striver Cp Sheet

---
+++ Codeforces/1199B. Water Lily.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double L,H;
+      cin>>H>>L;
+      cout<<fixed<<setprecision(13)<<( ( L*L - H*H) / ( 2 * H));
+
+
+
+
+
+
+    return 0;
+}
