bf03407a242a457da8fc79aa93a8568be31e9f97
Md Imon Hossain <imoncoc0@gmail.com>
2022-10-27 10:59:29 +0600

    Striver Cp sheet

---
+++ Codeforces/688B. Lovely Palindromes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    string res = s;
+    reverse(s.begin(), s.end());
+
+    cout<<res+s<<endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
