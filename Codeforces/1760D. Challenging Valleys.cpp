b17118b0fb66b8dbdeb481ad07374dfecff247e3
Md Imon Hossain <imoncoc0@gmail.com>
2022-11-24 10:49:33 +0600

    835 div 4

--- Codeforces/1760D. Challenging Valleys.cpp
+++ Codeforces/1760D. Challenging Valleys.cpp
@@ -10,39 +10,34 @@ void solve(){
         arr.push_back(x);
     }
 
-    int left = 0, right = n - 1, count = 1;
-    bool flag = false;
+    int count = 0, cons = 0, l = 0;
 
-    for(int i = 1; i < n; i++){
-        if(arr[i] >= arr[i-1]) count++;
-    }
-    //cout<<"count: "<<count<<endl;
-
-    if(count == n) flag = true;
-    int prevLeft = arr[left++], prevRight =  arr[right--];
-    //cout<<"prevLeft: "<<prevLeft<<endl;
-    //cout<<"prevRight: "<<prevRight<<endl;
-
-
-    while(left <= right){
-            //cout<<"prevLeft: "<<prevLeft<<endl;
-            //cout<<"prevRight: "<<prevRight<<endl;
-        if(arr[left] <= prevLeft){
-                prevLeft = arr[left];
-            left++;
+    for(int i = 0; i < n; i++){
+        cons++;
+        if(i == n-1){
+            if(l == 0 || arr[l-1] > arr[l] ){
+            count++;
+            }
         }
-        if(arr[right] <= prevRight){
-                prevRight = arr[right];
-            right--;
+        else{
+            if( arr[i+1] == arr[i] ) {
+                continue;
+            }
+            else if( arr[i+1] < arr[i] ) {
+                l = i+1;
+                cons = 0;
+            }
+            else{
+                if(l == 0 || arr[l-1] > arr[l]){
+                    count++;
+                }
+                l = i +1;
+                cons = 0;
+            }
         }
     }
 
-    //cout<<"prevLeft: "<<prevLeft<<endl;
-    //cout<<"prevRight: "<<prevRight<<endl;
-
-
-
-    if(flag == true || prevLeft == prevRight){
+    if(count == 1){
         cout<<"YES"<<endl;
     }
     else{
