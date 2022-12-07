decd3fb7e3ddc75007d2a87a2f8684c1ce3c0841
Md Imon Hossain <imoncoc0@gmail.com>
2022-12-07 11:02:30 +0600

    Solved -> 1372A

---
+++ Codeforces/1372A. Omkar and Completion.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+    int n;
+    cin>>n;
+    for(int i = 1; i <= n; i++) {
+        cout<<1<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
+
