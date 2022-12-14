2094ded0805f165191cdf20c851a31e77211d75f
Md Imon Hossain <imoncoc0@gmail.com>
2022-12-14 12:05:40 +0600

    Solved -> 1766A

--- Codeforces/1766A. Extremely Round.cpp
+++ Codeforces/1766A. Extremely Round.cpp
@@ -4,23 +4,37 @@ using namespace std;
 void solve(){
     int n;
     cin>>n;
-    vector<int> arr(n);
-    for(int i = 0; i < n; i++){
-        cin>>arr[i];
+    long long count = 0;
+    for(long long i = 1; i <= n; i++){
+        if(i <= 9) count++;
+        else if(i >= 10 && i < 100 && i % 10 == 0 ){
+            count++;
+            i += 9;
+        }
+        else if(i >= 100 && i < 1000 && i % 100 == 0){
+            count++;
+            i += 99;
+        }
+        else if(i >= 1000 && i < 10000 && i % 1000 == 0){
+            count++;
+            i += 999;
+        }
+        else if(i >= 10000 && i < 100000 && i % 10000 == 0){
+            count++;
+            i += 9999;
+        }
+        else if(i >= 100000 && i < 1000000 && i % 100000 == 0){
+            count++;
+            i += 99999;
+        }
+       // else if(||  || i % 1000 == 0 || i % 10000 == 0 || i % 100000 == 0)
     }
 
-    for(int i = 0; i < n; i++){
-        if(arr[i] % 10 == 0){
-            cout<<arr[i]<<endl;
-        }
-        else{
-            cout<<arr[i] % 10<<endl;
-        }
-    }
+    cout<<count<<endl;
 }
 int main(){
-    int t = 1;
-    //cin>>t;
+    int t ;
+    cin>>t;
     while(t--){
         solve();
     }
