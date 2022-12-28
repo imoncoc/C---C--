844df1088e3c604dc631017a12c55b0c5a23c900
Md Imon Hossain <imoncoc0@gmail.com>
2022-12-28 11:53:52 +0600

    Solved -> 1774A

--- Codeforces/1774A. Add Plus Minus Sign.cpp
+++ Codeforces/1774A. Add Plus Minus Sign.cpp
@@ -4,62 +4,37 @@ using namespace std;
 void solve(){
     int n;
     cin>>n;
-    string s, ans;
-    cin>>s;
-    int count = 0;
-    bool flag = true;
+    string a, ans;
+    cin>>a;
+    int one = 0;
 
-    for(int i = 0; i < n-1; i++){
-            if(s[i] == '1') count++;
-        if(s[i] == '1' && s[i+1] == '1' && flag == true){
-            ans += '-';
-            flag = false;
-        }
-        else if(s[i] == '1' && flag == false){
-            ans += '+';
-            flag = true;
-        }
-        else{
-            ans += '+';
-        }
-    }
-
-    if(count % 2 == 1 && s[n-1] == '1'){
-        ans[ans.size()-1] = '-';
-    }
-
-    cout<<"count: "<<count<<endl;
-    cout<<ans<<endl;
-
-    /*
-    int len = s.size();
-    for(int i = 0; i < n; i++ ){
-        if(s[i] == '1') count++;
-    }
-
-    if(count == 1){
-        for(int i = 0; i < len-1; i++){
-            ans += '+';
-        }
-    }
-    else{
-        for(int i = 0; i < len-1; i++){
-            if(s[i] == '1' && flag){
-                ans += '-';
-                flag = false;
-            }
-            else if(s[i] == '1' && !flag){
-                ans += '+';
-                flag = true;
+    for(int i = 0; i < n; i++)
+        {
+            if(i == 0){
+                if(a[i] == '1'){
+                    one++;
+                }
             }
             else{
-                ans += '+';
+                if(a[i] == '1'){
+                    one++;
+
+                    if( (one % 2 ) == 0 ){
+                        ans += '-';
+                    }
+                    else{
+                       ans += '+';
+                    }
+                }else{
+                    ans += '+';
+                }
             }
         }
-    }
+
+
 
     cout<<ans<<endl;
-    */
+
 }
 int main(){
     int t;
