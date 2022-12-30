c89fa53174a4d335224d59a91c0334c209112621
Md Imon Hossain <imoncoc0@gmail.com>
2022-12-30 15:29:34 +0600

    Solved -> 1731A

---
+++ Codeforces/1731A. Joey Takes Money.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+    ll n;
+    cin>>n;
+    ll ans = 1;
+    for(ll i = 0; i < n; i++){
+        ll x;
+        cin>>x;
+        ans = ans * x;
+    }
+    cout<<( (ans) + (n - 1) )*2022<<endl;
+}
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
+
