a3051cb072bc024db923167f7e34a995e4b507ac
Md Imon Hossain <imoncoc0@gmail.com>
2023-01-06 11:03:35 +0600

    Solved -> 1768A

---
+++ Codeforces/1768A. Greatest Convex.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+    int n;
+    cin>>n;
+    cout<<n-1<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
+
