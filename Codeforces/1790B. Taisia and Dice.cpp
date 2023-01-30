6a1b2a798ac56864e451173c0d502c1a4f0c125e
Md Imon Hossain <imoncoc0@gmail.com>
2023-01-30 11:56:30 +0600

    Solved -> 1790B

--- Codeforces/1790B. Taisia and Dice.cpp
+++ Codeforces/1790B. Taisia and Dice.cpp
@@ -3,12 +3,29 @@ using namespace std;
 #define ll long long int
 void solve(){
     int n, s, r;
-     cin >> n >> s >> r;
-        int max_dice = (s - r + n - 2) / (n - 1);
-        for (int i = 0; i < n - 1; i++) {
-            cout << max_dice + (i < (s - r) % (n - 1)) << " ";
+     cin>>n>>s>>r;
+     int max_dice = 0, divite = 0, mod = 0;
+
+      max_dice = s - r;
+      divite = r / (n-1);
+      mod = r % (n-1);
+     //cout<<"max_dice: "<<max_dice<<endl;
+     //cout<<"divite: "<<divite<<endl;
+     //cout<<"mod: "<<mod<<endl;
+
+     cout<<max_dice<<" ";
+     for(int i = 0;  i< n-1; i++){
+        if(mod > 0){
+            cout<<divite+1<<" ";
         }
-        cout << max_dice + (s - r + n - 2) / (n - 1) << endl;
+        else{
+            cout<<divite<<" ";
+        }
+        mod--;
+     }
+
+     cout<<endl;
+
 }
 int main(){
     int t;
