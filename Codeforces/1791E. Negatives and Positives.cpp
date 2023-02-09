20c534b296550b769ccee1c11e1b54c4dccdcb9f
Md Imon Hossain <imoncoc0@gmail.com>
2023-02-09 21:00:21 +0600

    Unsolved -> 1791E

--- Codeforces/1791E. Negatives and Positives.cpp
+++ Codeforces/1791E. Negatives and Positives.cpp
@@ -2,6 +2,42 @@
 using namespace std;
 #define ll long long int
 void solve(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    int sum = 0;
+
+
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+        sum += arr[i];
+    }
+    int ans = sum;
+
+    //cout<<"sum: "<<sum<<endl;
+
+    for(int i = 0; i < n-1; i++){
+            int res = 0;
+
+        for(int j = i; j < i+2; j++ ){
+            //cout<<arr[j]<<" ";
+            res += (arr[j] * -1);
+            res += (arr[j] * -1);
+        }
+         res += sum;
+         ans = max(res, ans);
+        //cout<<endl;
+        //cout<<"res: "<<res<<endl;
+    }
+    //cout<<endl;
+
+    //cout<<"ans: "<<ans<<endl;
+
+
+
+    cout<<ans<<endl;
+
+
 }
 int main(){
     int t;
