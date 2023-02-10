dbfd2694e2d3dd23d7ce3f87f39a42601171345b
Md Imon Hossain <imoncoc0@gmail.com>
2023-02-10 10:23:11 +0600

    Solved -> 1791E

--- Codeforces/1791E. Negatives and Positives.cpp
+++ Codeforces/1791E. Negatives and Positives.cpp
@@ -4,36 +4,25 @@ using namespace std;
 void solve(){
     int n;
     cin>>n;
-    vector<int> arr(n);
-    int sum = 0;
-
+    vector<ll> arr(n);
+    ll neg = 0, ans = 0, mini = INT_MAX;
 
     for(int i = 0; i < n; i++){
         cin>>arr[i];
-        sum += arr[i];
-    }
-    int ans = sum;
-
-    //cout<<"sum: "<<sum<<endl;
-
-    for(int i = 0; i < n-1; i++){
-            int res = 0;
-
-        for(int j = i; j < i+2; j++ ){
-            //cout<<arr[j]<<" ";
-            res += (arr[j] * -1);
-            res += (arr[j] * -1);
+        if(arr[i] < 0){
+            neg++;
         }
-         res += sum;
-         ans = max(res, ans);
-        //cout<<endl;
-        //cout<<"res: "<<res<<endl;
+        ans += abs(arr[i]);
+        mini = min(mini, abs(arr[i]));
     }
-    //cout<<endl;
 
+    //cout<<"neg: "<<neg<<endl;
     //cout<<"ans: "<<ans<<endl;
+    //cout<<"mini: "<<mini<<endl;
 
-
+    if(neg % 2 == 1){
+        ans -= (2 * mini);
+    }
 
     cout<<ans<<endl;
 
