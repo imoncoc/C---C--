60ac74ab14e0b454522c7624cba4390db59d24e0
Md Imon Hossain <imoncoc0@gmail.com>
2023-02-14 09:45:22 +0600

    Wrong submitted

---
+++ Codeforces/1793A. Yet Another Promotion.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+    ll a, b, n, m;
+    cin>>a>>b;
+    cin>>n>>m;
+    cout<<a+b<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
+
