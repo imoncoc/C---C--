951094ab524eda13cebc028b6ba1ca5bc7a95de1
Md Imon Hossain <imoncoc0@gmail.com>
2023-02-15 20:35:29 +0600

    Solved -> 1793A

--- Codeforces/1793A. Yet Another Promotion.cpp
+++ Codeforces/1793A. Yet Another Promotion.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 #define ll long long int
 void solve(){
-    ll a, b, n, m;
-    cin>>a>>b;
-    cin>>n>>m;
-    cout<<a+b<<endl;
+    long long a,b,n,m;
+    cin>>a>>b>>n>>m;
+    if(n<=m){
+        cout<<n*min(a,b)<<endl;
+        return;
+    }
+
+    if((m*a)<=(m+1)*b){
+        long long temp1=n/(m+1);
+        long long temp2=n%(m+1);
+        long long temp3=temp1*m*a;
+        // cout<<"temp1 is "<<temp1<<endl;
+        // cout<<"temp2 is "<<temp2<<endl;
+        // cout<<"temp3 is "<<temp3<<endl;
+        temp3+=temp2*min(a,b);
+        cout<<temp3<<endl;
+    }
+    else{
+        cout<<n*b<<endl;
+    }
+    return;
 }
 int main(){
     int t;
