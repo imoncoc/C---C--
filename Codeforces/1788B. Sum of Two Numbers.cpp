44e0fc69b73e81e7d73c85e860e4496956ed8029
Md Imon Hossain <imoncoc0@gmail.com>
2023-02-22 17:47:19 +0600

    Solved -> 1788B

--- Codeforces/1788B. Sum of Two Numbers.cpp
+++ Codeforces/1788B. Sum of Two Numbers.cpp
@@ -3,21 +3,34 @@ using namespace std;
 #define ll long long int
 void solve(){
     int n;
-    cin>>n;
-    if(n == 1){
-        cout<<1<<" "<<0<<endl;
-        return;
-    }
-    if(n == 19){
-        cout<<14<<" "<<5<<endl;
-        return;
-    }
-    if(n == 99){
-        cout<<45<<" "<<53<<endl;
-        return;
-    }
-    int sum = n / 2;
-    cout<<sum<<" "<<n-sum<<endl;
+		cin>>n;
+		int n1=0,n2=0;
+		int counter=0;
+		int a=1;
+		while(n>0)
+		{
+			int rem = n % 10;
+			if( rem % 2 == 0)
+			{
+				n1+=a*( rem /2);
+				n2+=a*( rem /2);
+			}
+			else if( rem  % 2 != 0 && counter%2 ==0)
+			{
+				n1+=a * (rem +1)/2;
+				n2+=a * (rem-1) /2;
+				counter++;
+			}
+			else
+			{
+				n1+=a* (rem-1) /2;
+				n2+=a* (rem +1)/2;
+				counter++;
+			}
+			a*=10;
+			n/=10;
+		}
+		cout<<n1<<" "<<n2<<endl;
 }
 int main(){
     int t;
