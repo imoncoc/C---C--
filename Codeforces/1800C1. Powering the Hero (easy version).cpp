1d21fbffa1cab7d3d2a92441f12d456ba2256001
Md Imon Hossain <imoncoc0@gmail.com>
2023-03-09 12:51:27 +0600

    Solved -> 1800C1

---
+++ Codeforces/1800C1. Powering the Hero (easy version).cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+    int n;
+    cin>>n;
+    priority_queue<int> pq;
+    ll ans = 0;
+    while(n--){
+        int x;
+        cin>>x;
+        if(x == 0){
+            if(!pq.empty()){
+               ans += pq.top();
+               pq.pop();
+            }
+        }
+        else{
+            pq.push(x);
+        }
+
+    }
+
+    //cout<<"ans: "<<ans<<endl;
+    cout<<ans<<endl;
+    /*
+    cout << "Priority Queue: ";
+    while (!pq.empty()) {
+        cout << pq.top() << ' ';
+        pq.pop();
+    }
+    */
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
+
+
