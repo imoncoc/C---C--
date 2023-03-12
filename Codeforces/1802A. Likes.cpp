e41fd982998c9b133e42b51a6a51a523c49c87de
Md Imon Hossain <imoncoc0@gmail.com>
2023-03-12 12:06:31 +0600

    Solved -> 1802A

--- Codeforces/1802A. Likes.cpp
+++ Codeforces/1802A. Likes.cpp
@@ -2,6 +2,49 @@
 using namespace std;
 #define ll long long int
 void solve(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    int mx = INT_MIN;
+    int pos = 0, neg = 0;
+
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+        mx = max(arr[i], mx);
+        if(arr[i] > 0) pos++;
+        else neg++;
+    }
+
+    //cout<<"mx: "<<mx<<endl;
+
+    //sort(arr.begin(), arr.end(), greater<int>());
+
+    for(int i = 1; i <= n; i++){
+            if(i > pos){
+                pos--;
+                cout<<pos<<" ";
+            }
+            else{
+                cout<<i<<" ";
+            }
+
+    }
+    cout<<endl;
+
+    int a = 0;
+    for(int i = 0; i < n; i++){
+            if(neg != 0){
+                cout<<1<<" "<<0<<" ";
+                neg--;
+                i++;
+                }
+                else{
+                        a++;
+                    cout<<a<<" ";
+                }
+
+    }
+    cout<<endl;
 }
 int main(){
     int t;
