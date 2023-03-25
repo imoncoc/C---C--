e9613cfc022ceb055cd744400ab7a0a838f09a69
Md Imon Hossain <imoncoc0@gmail.com>
2023-03-25 22:53:37 +0600

    Wrong submitted -> 1809B

---
+++ Codeforces/1809B. Points on Plane.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+    ll n;
+    cin>>n;
+    cout<<sqrt(n)<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+
+    return 0;
+}
+
