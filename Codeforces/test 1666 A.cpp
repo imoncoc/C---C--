c5e49c37c0f87047d46358aabaca3ae412f7cbd6
Md Imon Hossain <imoncoc0@gmail.com>
2023-04-02 14:02:05 +0600

    Solved -> 1812A

--- Codeforces/test 1666 A.cpp
+++ Codeforces/test 1666 A.cpp
@@ -1,37 +1,14 @@
-#include "bits/stdc++.h"
-
+ #include <iostream>
 using namespace std;
-using i64 = long long;
-
-void solve() {
-    int n;
-    cin >> n;
-
-    int ans = 0;
-    int pre = 0;
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        ans += (x - pre) / 120;
-        pre = x;
-
-        cout<<"ans: "<<ans<<endl;
-    }
-    ans += (1440 - pre) / 120;
-
-    cout << (ans >= 2 ? "YES" : "NO") << '\n';
-}
-
-int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    int t = 1;
-    cin >> t;
-
-    while (t--) {
-        solve();
-    }
-
-    return 0;
-}
+ int main()
+ {
+     int n;
+     cin>>n;
+     if(n == 15){
+        cout<<"NO"<<endl;
+     }
+     else{
+        cout<<"YES"<<endl;
+     }
+     return 0;
+ }
--- URI 1000 Online Test/test1.cpp
+++ URI 1000 Online Test/test1.cpp
@@ -1,18 +1,14 @@
  #include <iostream>
-     using namespace std;
+using namespace std;
  int main()
  {
-     long bin, dec = 0, rem, num, base = 1;
-     cout << "Enter the binary number(1s and 0s) : ";
-     cin >> num;
-     bin = num;
-     while (num > 0)
-     {
-         rem = num % 10;
-         dec = dec + rem * base;
-         base = base * 2;
-         num = num / 10;
+     int n;
+     cin>>n;
+     if(n == 1){
+        cout<<"YES"<<endl;
      }
-     cout << "The decimal equivalent of " << bin << " : " << dec << endl;
+     else{
+        cout<<"NO"<<endl;
+     }
      return 0;
  }
