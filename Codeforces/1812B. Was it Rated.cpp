2952a785a211db6613fb6385632fdd9e02805b7c
Md Imon Hossain <imoncoc0@gmail.com>
2023-04-03 12:20:26 +0600

    Solved -> 1812B

---
+++ Codeforces/1812B. Was it Rated.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+using namespace std;
+ int main()
+ {
+     int n;
+     cin>>n;
+     if(n == 15 || n == 20 || n == 21){
+        cout<<"NO"<<endl;
+     }
+     else{
+        cout<<"YES"<<endl;
+     }
+     return 0;
+ }
+
